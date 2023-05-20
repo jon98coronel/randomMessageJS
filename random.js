ff564655cf9c514fca17f8967d2b18643e2dd2ae
JonCore <joncore98@gmail.com>
2023-05-19 19:40:33 -0700

    First commit

---
+++ random.js
@@ -0,0 +1,4 @@
+  const randomMessage = ['Go outside and get some fresh air.', 'Stay home and watch tv all day', 'What do you feel like eating?', 'You are going to have an amazing day!'];
+  const random = randomMessage[(Math.floor(Math.random() * randomMessage.length))];
+  console.log(random);
+ 
